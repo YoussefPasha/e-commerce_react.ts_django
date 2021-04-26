25317c4e51fa65b8bc8ba50e13dc93b69f80dbde
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-04-26 20:44:40 +0200

    add redux actions and reducers re state our state and add loading and message alert

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,29 +1,36 @@
-import React, { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import { Product } from "../components";
+import { Product, Loader, Message } from "../components";
 import productInterface from "../models/productTypes";
-import axios from "axios";
-const HomeScreen = () => {
-  const [products, setProducts] = useState<Array<productInterface>>([]);
+import * as ProductActions from "../actions/ProductActions";
+import { useDispatch, useSelector } from "react-redux";
 
+const HomeScreen = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    const fetch = async () => {
-      const { data } = await axios.get("/api/products/");
-      setProducts(data);
-    };
-    fetch();
-  }, []);
+    dispatch(ProductActions.listProducts());
+  }, [dispatch]);
+
+  const ProductsState = useSelector((state: any) => state.productList);
+
+  const { error, loading, products } = ProductsState;
 
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product: productInterface) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant={"danger"}>{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product: productInterface) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </div>
   );
 };
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,8 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import Thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { productListReducers } from "./reducers/ProductReducers";
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducers,
+});
 
 const initialState = {};
 
