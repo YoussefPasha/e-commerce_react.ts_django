647a1d952e63bc45aecc86652dbb5e5248454d57
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-05-04 01:28:49 +0200

    add authntication backend

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('',views.getRoutes , name="routes"),
+    path('users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('', views.getRoutes, name="routes"),
+    path('users/profile', views.getUserProfile, name="users_profile"),
     path('products/', views.getProducts, name="products"),
-    path('products/<str:pk>',views.getProduct , name="product"),
-    
+    path('products/<str:pk>', views.getProduct, name="product"),
+
 ]
